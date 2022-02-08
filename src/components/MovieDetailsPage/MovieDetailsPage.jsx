49b7a503e902989ba8fa45ec215deae1cba74bd8
Vsevolod1001 <sevant.101@gmail.com>
2022-02-08 23:04:01 +0200

    1

--- src/components/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/components/MovieDetailsPage/MovieDetailsPage.jsx
@@ -2,9 +2,11 @@ import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react";
 import { getPublicationId } from "services/publicationsApi";
 
+
+
 export const MovieDetailsPage = () => {
     const {movieId} = useParams();
-    const [item, setItem] = useState(null);
+    const [item, setItem] = useState([]);
     // const o = getPublicationId(movieId);
     // console.log(o)
     useEffect(() => {
@@ -24,11 +26,18 @@ export const MovieDetailsPage = () => {
     
     return (
         <div>
-             <h1>{movieId}</h1>
-             {/* <h2>{item.title}</h2> */}
-            
-            
-            {/* {item && item.map(card => (<li key={card.id}>{card.title}</li>))} */}
+            <div><img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.title} /></div>
+            <div> 
+                <h1>{movieId}</h1>
+                <h2>{item.title}({item.release_date})</h2>
+             
+                <h3>Overview:</h3>
+                <p>{item.overview}</p>
+                <h3>Genres:</h3>
+             {/* {item.genres.map(genre => (
+                 <li>{genre.name}</li>
+             ))} */}
+             </div>
         </div>
     )
 }
